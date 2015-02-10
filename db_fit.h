9eeeabbcbc9b0f7b145e30f5d319f94a142e6337
Kamal Singhrao <ksinghrao@icloud.com>
2015-02-09 19:25:05 -0700

    test1

---
+++ db_fit.h
@@ -0,0 +1,17 @@
+#ifndef db_fit.h
+#define db_fit.h
+
+
+void control_fct();
+
+void comp_spectrum();
+
+void pmt_loop();
+
+
+double lin_spectra_fct(double *x, double *p);
+
+string convertInt(int number);
+
+
+#endif
